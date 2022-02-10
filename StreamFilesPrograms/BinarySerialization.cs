3177f22d0d48fa93277a8d6c6fb08a4124b36f98
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-10 17:46:00 +0530

    [Raja}Add Fili I/O stream problems implemented in StreamFile

---
+++ StreamFilesPrograms/BinarySerialization.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamFilesPrograms
+{/// <summary>
+/// Serialisation and Deserialisation
+/// </summary>
+    class BinarySerialization
+    {
+        //Binary Serialisation Method
+        public static void Serialisation()//this method is used for serialisation
+        {
+            string path = @"D:\git\C#StreamFiles\C-StreamFiles\StreamFilesPrograms\Sample.txt";
+            Person person = new Person();
+            FileStream fileStream = new FileStream(path, FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fileStream, person);
+        }
+        public static void Deserialisation()//this method used for deserialisation
+        {
+            string path = @"D:\git\C#StreamFiles\C-StreamFiles\StreamFilesPrograms\Sample.txt";
+            FileStream fileStream = new FileStream(path, FileMode.Open);
+            BinaryFormatter formatter = new BinaryFormatter();
+            Person person=(Person)formatter.Deserialize(fileStream);
+            Console.WriteLine($"Person:{person.Name}    Id :{ person.Id}");
+        }
+    }
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; } = "Raja";
+        public int Id { get; set; } = 152507;
+    }
+}
---
+++ StreamFilesPrograms/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamFilesPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exit();
+            //ReadFile();
+            //CopyFile();
+            //DeleteFile();
+            //StreamReader();
+            //StreamWriter();
+            BinarySerialization.Serialisation();
+            BinarySerialization.Deserialisation();
+        }
+        public static void Exit()//To know the File is Exist or not
+        {
+            string path = @"D:\git\C#StreamFiles\C-StreamFiles\StreamFilesPrograms\Sample.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File is present");
+            }
+            else
+            {
+                Console.WriteLine("File is not found");
+            }
+        }
+        public static void ReadFile()//it is used to Read the files
+        {
+            string path = @"D:\git\C#StreamFiles\C-StreamFiles\StreamFilesPrograms\Sample.txt";
+            string read = File.ReadAllText(path);
+            Console.WriteLine(read);
+            Console.ReadKey();
+        }
+        public static void CopyFile()//this method for copy the file from one file to another
+        {
+            string path = @"D:\git\C#StreamFiles\C-StreamFiles\StreamFilesPrograms\Sample.txt";
+            string CopyPath = @"D:\git\C#StreamFiles\C-StreamFiles\StreamFilesPrograms\Sample1.txt";
+            File.Copy(path, CopyPath);
+            Console.ReadKey();
+        }
+        public static void DeleteFile()//this method for deleting the file
+        {
+            string CopyPath = @"D:\git\C#StreamFiles\C-StreamFiles\StreamFilesPrograms\Sample1.txt";
+            File.Delete(CopyPath);
+            Console.WriteLine("The file deleted suceefully");
+            Console.ReadKey();
+        }
+        public static void StreamReader()//It is used to read the stream file
+        {
+            string path = @"D:\git\C#StreamFiles\C-StreamFiles\StreamFilesPrograms\Sample.txt";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string line = "";
+                if ((line=sr.ReadLine())!= null)
+                {
+                    Console.WriteLine(line);
+                }
+                Console.ReadKey();
+            }
+        }
+        public static void StreamWriter()//It is used to write the files 
+        {
+            string path = @"D:\git\C#StreamFiles\C-StreamFiles\StreamFilesPrograms\Sample.txt";
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("Hello Raja");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+    }
+}
